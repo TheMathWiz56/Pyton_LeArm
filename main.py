a49540d4559563902335d717d8fc4ade40eb3ac5
TheMathWiz56 <amadorjosephg@gmail.com>
2024-04-23 09:58:56 -0500

    First Structure idea

--- main.py
+++ main.py
@@ -5,6 +5,29 @@ except ModuleNotFoundError:
 import board
 import busio
 
+"""
+------------------------
+Plan for arm control
+------------------------
+
+present Option at start of the program to run simulation or run actual arm (changes how classes are initialized and run)
+
+Main.py - has arm.py for setting desired position and returning forward kinematics
+    Also read's joysticks and modifies desired position accordingly
+arm.py - Puts all the link.py's together. Has inverse and forwards kinematic functions and periodically sets servo 
+    angles on another thread
+link.py - creates transform matrix for that link given link parameters and contains the relevant servo instance
+constants.py - stores all constant values like indexes and offsets
+
+To Do List:
+First find offset for each servo and valid range
+Joysticks to update inverse kinematics (One joystick changes XY component, DPad does Z and other joystick does wrist, 
+    bumper does claw)
+create and apply inverse kinematics
+    Create smooth pathing function
+allow autonomous operations to be stored and run from the commmand line 
+"""
+
 
 def main():
     print("Hello World")
