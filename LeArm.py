38585b239d962b784da2315e64a476c8bf157f5e
TheMathWiz56 <amadorjosephg@gmail.com>
2024-04-26 15:49:40 -0500

    Actuation Range

--- LeArm.py
+++ LeArm.py
@@ -19,7 +19,7 @@ class Arm:
 
         self.kit.servo[LeArmConstants.PINS.SHOULDER.value].set_pulse_width_range(500, 2500)
         self.kit.servo[LeArmConstants.PINS.ELBOW1.value].set_pulse_width_range(625, 2750)
-        self.kit.servo[LeArmConstants.PINS.ELBOW2.value].set_pulse_width_range(500, 2500)
+        self.kit.servo[LeArmConstants.PINS.ELBOW2.value].set_pulse_width_range(625, 2850)
 
         self.update_base_to_wrist_frame_transformation()
 
