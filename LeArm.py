9df587d533fb253923300a846562e58584184212
TheMathWiz56 <amadorjosephg@gmail.com>
2024-06-22 13:51:10 -0500

    Cube #4. Gradual pitch and roll change

--- LeArm.py
+++ LeArm.py
@@ -311,14 +311,17 @@ class Arm:
             # Do gripper last
             self.set_setpoint_to_servo_raw(m.degrees(self.current_setpoint.theta6), LeArmConstants.PINS.GRIPPER.value)
         elif command_type == LeArmConstants.CommandType.TRAVEL_AT_HEIGHT.value:
+            steps = 3
+            dp = (pitch - self.current_setpoint.pitch) / steps
+            dr = (roll - self.current_setpoint.roll) / steps
 
             self.go_to(get_gripper_state_from_angle_rad(self.current_setpoint.theta6),
                        LeArmConstants.CommandType.STEPPED.value, self.current_setpoint.x, self.current_setpoint.y,
-                       LeArmConstants.CRUISING_HEIGHT, pitch, roll)
+                       LeArmConstants.CRUISING_HEIGHT, self.current_setpoint.pitch + dp, self.current_setpoint.roll + dr)
             self.go_to(get_gripper_state_from_angle_rad(self.current_setpoint.theta6),
                        LeArmConstants.CommandType.STEPPED.value, x, y,
-                       LeArmConstants.CRUISING_HEIGHT, pitch, roll)
-            self.go_to(gripper_setpoint, LeArmConstants.CommandType.STEPPED.value, x, y, z, pitch, roll)
+                       LeArmConstants.CRUISING_HEIGHT, self.current_setpoint.pitch + dp, self.current_setpoint.roll + dr)
+            self.go_to(gripper_setpoint, LeArmConstants.CommandType.STEPPED.value, x, y, z, self.current_setpoint.pitch + dp, self.current_setpoint.roll + dr)
         else:
             self.kinematics.solve(x, y, z, pitch, roll, LeArmConstants.gripper_positions[gripper_setpoint],
                                   command_type)
